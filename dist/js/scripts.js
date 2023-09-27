8fab70aa143aaac4d06b4445cafd7d08c5a1ff50
Spyder <139022731+spyder227@users.noreply.github.com>
2023-09-26 23:00:01 -0400

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,4 +1,4 @@
-const sheet = `https://opensheet.elk.sh/1rblIWd8uyKP0sX1fLCBodSMzw09hwdD-_vdXZm53fwA`;
+const sheet = `https://opensheet.elk.sh/1XxVPu8z9VHGfMVuS8oaoqT_r8CGLbvFlcSAJWyMARHg`;
 let threads = [];
 var filters = {};
 const typeSearch = `#quicksearch`;
