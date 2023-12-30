391f0963a95d6dbd161258c7fa36abcb73d1695c
spyder227 <rickard.nicole@hotmail.com>
2023-12-30 05:14:15 -0500

    wip index

---
+++ dist/js/minicharts.js
@@ -0,0 +1,101 @@
+function loadThreads(data) {
+    let threads = [];
+    data.filter(item => item.Status.toLowerCase().trim() !== 'archived').forEach(item => {
+        let delayClass = getDelay(item.LastUpdated);
+        let thread = {
+            status: item.Status ? item.Status.toLowerCase().trim() : '',
+            delay: delayClass,
+        }
+        threads = [...threads, thread];
+    });
+
+    return threads;
+}
+
+function loadCharts(threads) {
+    let statusChart = new ApexCharts(document.querySelector(".chart--status"), configStatus(threads));
+    statusChart.render();
+
+    window.addEventListener('resize', () => {
+        let statusChart = new ApexCharts(document.querySelector(".chart--status"), configStatus(threads));
+        statusChart.render();
+    });
+}
+function configStatus(threads) {
+    let owing = threads.filter(thread => thread.status === 'mine' || thread.status === 'start').length;
+    let active = threads.filter(thread => thread.status === 'theirs' || thread.status === 'upcoming').length;
+    let complete = threads.filter(thread => thread.status === 'complete').length;
+    let statusConfig = {
+        series: [owing, active, complete],
+        labels: ['Mine', 'Theirs', 'Completed'],
+        colors: ['rgba(162, 129, 119, 1)', 'rgba(125, 159, 129, 1)', 'rgb(141, 165, 176)'],
+        chart: {
+            type: 'donut',
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    size: '50%',
+                }
+            }
+        },
+        states: {
+            hover: {
+                filter: {
+                    type: 'none',
+                    value: 0,
+                }
+            },
+            active: {
+                filter: {
+                    type: 'none',
+                    value: 0,
+                }
+            },
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function(value, { seriesIndex, dataPointIndex, w }) {
+                return w.config.series[seriesIndex]
+            },
+            style: {
+                fontSize: '20px',
+                fontFamily: 'var(--font-accent)',
+                fontWeight: '400'
+            },
+            dropShadow: {
+                enabled: false,
+            }
+        }, 
+        legend: {
+            fontSize: '14px',
+            fontFamily: 'var(--font-body)',
+            fontWeight: '400',
+            markers: {
+                width: '10px',
+                height: '10px',
+                offsetX: '-2px',
+            },
+        },
+        stroke: {
+            show: true,
+            colors: 'var(--bg-body)',
+        },
+        tooltip: {
+            enabled: false,
+        },
+        theme: {
+            palette: 'palette4',
+        },
+        responsive: [{
+            breakpoint: 560,
+            options: {
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+
+    return statusConfig;
+}
