48045eb1f5b0e3e9e67089c43bae4c16b32e76cd
spyder227 <rickard.nicole@hotmail.com>
2024-01-25 16:13:01 -0500

    tweaked filters

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1028,7 +1028,7 @@ function formatCharacter(data, characterFilters, baseUrl) {
         }
     }
 
-    return `<div class="${data.Character.split(' ')[0].toLowerCase()} spy-track grid-item ${characterFilters} canon-${data.FilterCanon} shipped-${data.FilterShipped} gender-${data.FilterGender} ${ageFilters} ${sitesPlayed} housing-${data.FilterHousing} residence-${data.FilterResidence} homeowner-${data.FilterOwner}">
+    return `<div class="${data.Character.split(' ')[0].toLowerCase()} spy-track grid-item ${characterFilters} canon-${data.FilterCanon} shipped-${data.FilterShipped} needs-${data.FilterNeeds}     gender-${data.FilterGender} ${ageFilters} ${sitesPlayed} housing-${data.FilterHousing} residence-${data.FilterResidence} homeowner-${data.FilterOwner}">
         <div class="character">
             <div class="character--image">
                 <img src="${data.Image}" />
