e1d27ee5a697af796c9622c17bcff650ba65c2b4
spyder227 <rickard.nicole@hotmail.com>
2024-07-06 04:15:18 -0400

    added thread tags

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -146,7 +146,7 @@ function formatOldThread(site, siteURL, status, character, feature, title, threa
         buttons = `<button onClick="changeStatus(this)" data-status="${status}" data-id="${threadID}" data-site="${site}" data-character="${character.split('#')[0]}">Change Turn</button>
         <button onClick="markComplete(this)" data-id="${threadID}" data-site="${site}" data-character="${character.split('#')[0]}">Mark Complete</button>`;
     }
-    let html = `<div class="thread spy-track grid-item status--${status} ${character.split(' ')[0].toLowerCase()} delay--${delayClass} type--${type.split(' ')[0]} ${partnerClasses} grid-item"><div class="thread--wrap">
+    let html = `<div class="thread spy-track grid-item status--${status} ${character.split(' ')[0].toLowerCase()} delay--${delayClass} type--${type.split(' ')[0]} ${tagClasses} ${partnerClasses} grid-item"><div class="thread--wrap">
         <a class="thread--character" href="${siteURL}/?showuser=${character.split('#')[1]}">${character.split('#')[0]}</a>
         <a href="${siteURL}/?showtopic=${threadID}&view=getnewpost" target="_blank" class="thread--title">${title}</a>
         <span class="thread--feature">ft. ${featuring}</span>
@@ -158,7 +158,11 @@ function formatOldThread(site, siteURL, status, character, feature, title, threa
 
     return html;
 }
-function formatThread(site, siteURL, status, character, feature, title, threadID, icDate, partnerObjects, type, lastPost, delayClass, directoryString, snippet) {
+function formatThread(site, siteURL, status, character, feature, title, threadID, icDate, partnerObjects, type, lastPost, delayClass, directoryString, snippet, tags) {
+    //set tags
+    let tagClasses = tags && tags !== '' ? tags.split(' ').map(tag => `tag--${tag}`).join(' ') : '';
+    console.log(tagClasses);
+    
     //set writing partners
     let partners = ``;
     let partnerClasses = ``;
@@ -212,7 +216,7 @@ function formatThread(site, siteURL, status, character, feature, title, threadID
     } else {
         buttons = `<div class="icon" title="${type}"></div>`;
     }
-    let html = `<div class="thread spy-track grid-item status--${status} ${character.split(' ')[0].toLowerCase()} delay--${delayClass} type--${type.split(' ')[0]} ${partnerClasses} ${ftClasses} grid-item">
+    let html = `<div class="thread spy-track grid-item status--${status} ${character.split(' ')[0].toLowerCase()} delay--${delayClass} type--${type.split(' ')[0]} ${tagClasses} ${partnerClasses} ${ftClasses} grid-item">
         <div class="thread--wrap">
             <div class="thread--main">
                 <a href="${siteURL}/?showtopic=${threadID}&view=getnewpost" target="_blank" class="thread--title">${title}</a>
@@ -349,6 +353,15 @@ function addThread(e) {
         update = `${month} ${day}, ${year}`,
         snippet = e.currentTarget.querySelector('#description').value ? e.currentTarget.querySelector('#description').value : '';
 
+        let tagsField = e.currentTarget.querySelector('#tags');
+        let tags = Array.from(tagsField.options).filter(function (option) {
+            return option.selected;
+        }).map(function (option) {
+            return option.value;
+        });
+
+        console.log(tags);
+
         let partners = document.querySelectorAll('.partner select');
         let partnerObjects = [];
         partners.forEach(partnerObj => {
@@ -383,7 +396,8 @@ function addThread(e) {
         'Partner': partner,
         'Type': type,
         'LastUpdated': update,
-        'Snippet': snippet
+        'Snippet': snippet,
+        'Tags': tags.join(' ')
     }, null, threadDeploy, e);
 }
 function populateThreads(array, siteObject) {
@@ -423,7 +437,8 @@ function populateThreads(array, siteObject) {
                             array[i].LastUpdated.toLowerCase(),
                             getDelay(array[i].LastUpdated),
                             siteObject.Directory,
-                            array[i].Snippet);
+                            array[i].Snippet,
+                            array[i].Tags);
     }
     document.querySelector('#tracker--rows').insertAdjacentHTML('beforeend', html);
 
